3d973d21e6fafd959857762cade8e18f0d95b83f
Cleber Varçal <cleber.varcal@varcalsys.com.br>
2015-11-22 03:07:45 -0200

    Revert "Implementado Context(EF), Classes Genericas Repository e Service"
    This reverts commit d98df2b66ff667401af481d77faaddbbe1996717.

--- src/CrossCutting.Ioc/BootStrapper.cs
+++ src/CrossCutting.Ioc/BootStrapper.cs
@@ -6,11 +6,6 @@ using System.Threading.Tasks;
 using CrossCutting.Identity.Configuration;
 using CrossCutting.Identity.Context;
 using CrossCutting.Identity.Models;
-using Domain.Contracts.Repositories;
-using Domain.Contracts.Services.Core;
-using Domain.Services.Core;
-using Infra.Data.AppContext;
-using Infra.Data.Repositories.Core;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using SimpleInjector;
@@ -25,10 +20,6 @@ namespace CrossCutting.Ioc
             container.RegisterPerWebRequest<IUserStore<ApplicationUser>>(() => new UserStore<ApplicationUser>(new ApplicationDbContext()));
             container.Register<ApplicationUserManager>(Lifestyle.Scoped);
             container.Register<ApplicationSignInManager>(Lifestyle.Scoped);
-
-            container.Register(typeof(IServiceBase<>), typeof(ServiceBase<>));
-
-            container.Register(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
         }
     }
 }
